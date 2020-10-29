3af3583e53b707b078085e0234fb555a2b6644dd
lucas.lmlok <lucas.lmlok@gmail.com>
2020-10-29 22:17:41 +0800

    remove unnecessaries

--- dist/util/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_SECRET = exports.DB_PASSWORD = exports.DB_USERNAME = exports.DB_DATABASE = exports.DB_HOST = void 0;
-exports.DB_HOST = "localhost";
-exports.DB_DATABASE = "life_tracker";
-exports.DB_USERNAME = "root";
-exports.DB_PASSWORD = "12lawliet";
-exports.JWT_SECRET = "T5M*wI$ONG4XCAIK*WV3h=r8y1S9rk1XA$fPCZdkgYyHiPxd^4d5X7jl8gkQh0y3";
--- dist/util/identifiers.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.STATUS_DELETED = exports.STATUS_INACTIVE = exports.STATUS_ACTIVE = void 0;
-exports.STATUS_ACTIVE = "active";
-exports.STATUS_INACTIVE = "inactive";
-exports.STATUS_DELETED = "deleted";
